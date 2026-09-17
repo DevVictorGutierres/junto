5cb03e948c81b0b46415d2fc328307dadc1135b9
DevVictorGutierres <devvictorgutierres@gmail.com>
2026-09-17 02:17:21 -0300

    feat: implement rateLimit and CorsSecurity

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,13 +6,34 @@ import userRouter from "./routes/user.routes.js";
 import projectRouter from "./routes/project.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import { errorHandlerMiddleware } from "./middlewares/error.middleware.js";
+import { rateLimit } from "express-rate-limit";
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  maxAge: 86400,
+  optionsSuccessStatus: 200 
+};
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    status: 429,
+    error: 'Limite de requisições atingido! Tente novamente mais tarde.'
+  },
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+});
+
+app.use(cors(corsOptions));
+app.use(limiter);
 app.use(express.json());
 
-app.get("/", (_, res) => {
+app.get("/health", (_, res) => {
     return res.status(200).json({
         application: "Junto",
         version: "1.0.0",
